43cbb0b4c378077f605edc63e8a5241eaeb39adf
Zarigueya <esnic1994@gmail.com>
2025-12-29 13:38:31 -0500

    Funcion de invertir, de obtener mayor y eliminar duplicados

--- src/main/java/net/javaguides/Main.java
+++ src/main/java/net/javaguides/Main.java
@@ -1,10 +1,17 @@
 package net.javaguides;
+import java.util.ArrayList;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println(parOImpar(3));
+        System.out.println(invertir("java"));
+        List<Integer> listaEnteros = List.of(10, 2, 8, 11, 5);
+        System.out.println(obtenerMayor(listaEnteros));
+        List<String> listaNombres = List.of("Daniel", "Jose", "Daniel", "Johan", "Jose");
+        System.out.println(eliminarDuplicados(listaNombres));
     }
 
     public static String parOImpar(int numero) {
@@ -13,5 +20,41 @@ public class Main {
         }else{
             return "Impar";
         }
+    }
+
+    public static String invertir(String texto) {
+        String invertido = "";
+        for (int i = texto.length()-1; i >= 0 ; i--) {
+            invertido += texto.charAt(i);
+        }
+        return invertido;
+    }
+
+    public static int obtenerMayor(List<Integer> numeros) {
+        int mayor = numeros.get(0);
+        for (int i = 1; i < numeros.size(); i++) {
+            if(mayor < numeros.get(i)){
+                mayor = numeros.get(i);
+            }
+        }
+        return mayor;
+    }
+
+    public static List<String> eliminarDuplicados(List<String> nombres) {
+        List<String> nuevalistaNombres = new ArrayList<>();
+        nuevalistaNombres.add(nombres.get(0));
+        boolean repetido = false;
+        for (int i = 0; i < nombres.size(); i++) {
+            repetido = false;
+            for (int j = 0; j < nuevalistaNombres.size(); j++) {
+                if(nombres.get(i).equals(nuevalistaNombres.get(j))){
+                    repetido = true;
+                }
+            }
+            if(!repetido){
+                nuevalistaNombres.add(nombres.get(i));
+            }
+        }
+        return nuevalistaNombres;
     }
 }
